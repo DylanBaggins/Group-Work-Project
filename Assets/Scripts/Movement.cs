46cdffab6635132e19846d34f000c6a102d78cba
Unknown <dylanrichardking@gmail.com>
2018-02-19 13:05:49 +0000

    Updated Master with Kyle's Work

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
+
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, y, 0);
+    }
+}
