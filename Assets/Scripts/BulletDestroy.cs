99d6e40920f87b28a89ed8660196724e3165d3a8
Dylan King <dylanrichardking@gmail.com>
2018-04-22 12:06:27 +0100

    Updated Work

---
+++ Assets/Scripts/BulletDestroy.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDestroy : MonoBehaviour {
+
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Text Healthdisplay;
+    public int maxHealth = 100;
+    public int curHealth = 100;
+
+    //initialization 
+    void Start() { }
+
+    // Update 
+    void Update()
+    {
+        if (curHealth < 1)
+        {
+            Destroy(gameObject);
+        }
+
+
+
+        Healthdisplay.text = curHealth.ToString();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("HostileBullet"))
+        {
+            curHealth -= 1;
+            Destroy(other.gameObject);
+        }
+    }
+}
